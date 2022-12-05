6e84d074aa11b815906fda85a53f7a086dc5fffa
sathya177 <47978849+sathya177@users.noreply.github.com>
2022-12-05 10:53:24 +0530

    Update _config.tf

--- lesson-2-sli-slo/exercises/starter/_config.tf
+++ lesson-2-sli-slo/exercises/starter/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-tscotto"
+     bucket = "udacity-tf-sathya-udacity"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
